ea09ff61dce83d3185aa594e6b021411ae83937a
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2021-03-04 03:55:50 -0700

    Replace Bootstrap nav with custom nav

--- src/Components/UserNav.js
+++ src/Components/UserNav.js
@@ -1,16 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import CredentialModal from './CredentialModal';
+// import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
-const UserNav = () => {
-
-
-return(
-    <Container className="bg-dark">
-
-    </Container>
-);
+const UserNav = ({ setUserData, userData }) => {
 
 
+    return (
+        <div className="user-nav--container">
+            <div className="user-nav">
+                <div className="user-nav--logo">Space Traders</div>
+                <div className="user-nav--content">
+                    <div className="user-nav--buttons mr-auto">
+                        <button className="btn btn-primary" href="#">Get User Info</button>
+                        <CredentialModal setUserData={setUserData} />
+                        <button className="btn btn-primary" href="#">Option 3</button>
+                    </div>
+                    <div className="user-nav--user-info--container">
+                        {console.log(userData)}
+                        <strong className="col-12 user-nav--username">{userData.username}</strong>
+                        <strong className="col-12 user-nav--money">$1,000</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default UserNav
