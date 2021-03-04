6d22a5f208c173f5b1fa577c2039e64e522b9377
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2021-03-04 03:45:56 -0700

    Save user data to local storage on load

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import UserNav from './Components/UserNav';
+import { useState, useEffect } from 'react';
 
 function App() {
 
+  //const[userCredentials, setUserCredentials] = useState({});
+  const[userData, setUserData] = useState({});
 
-
-
+  useEffect(() => {
+    if (localStorage.getItem("ST_UserData") !== null) {
+      console.log("Initial Load: There is data in local storage");
+      var data = JSON.parse(localStorage.getItem("ST_UserData"));
+      setUserData(data);
+    } else {
+      console.log("Initial Load: There is no data in local storage");
+      setUserData({ username: "Not Logged In" });
+    }
+  }, []);
 
 
   return (
     <div className="App">
-      <UserNav />
+      <UserNav setUserData={setUserData} userData={userData} />
     </div>
   );
 }
