9c2c04881f50ddbd2a42a27f6f2e49535611c42f
Ryan Wilson <ryan.law.wilson@gmail.com>
2021-09-13 11:35:25 -0600

    Refactor router logic to controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,5 @@
 // All of the handlers for the routes should go here.
+
+export const getPosts = (req, res) => {
+    res.send("This workds!");
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,14 @@
 import express from 'express';
 
+// The logic for each route is in the controllers folder and those functions get imported here.
+import { getPosts } from '../controllers/posts.js';
+
 // Need to initialize a router to start adding backend routes.
 const router = express.Router();
 
 // defines a function that gets run when someone visits that URL.
 // All routes will have a request (req) and a response (res).
-router.get('/', (req, res) => {
-    res.send("This workds!");
-});
+router.get('/', getPosts);
 
 // Export the router so that you can add it to the main express app in index.js.
 export default router;
