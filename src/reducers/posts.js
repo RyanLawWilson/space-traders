b625680692c6f97c6c0c24679e8e21c69c16853c
Ryan Wilson <ryan.law.wilson@gmail.com>
2021-09-16 13:45:25 -0600

    Fix bug that prevents posts to re-render when deleted

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,7 +1,5 @@
 // A reducer is a function that accepts the state and action.
 
-import Posts from "../Components/Posts/Posts";
-
 // The state must always be equal to something, so set initial value.
 // Since this is the only function being exported from this file, naming the function is not necessary.
 const reducer = (postsState = [], action) => {
@@ -13,6 +11,9 @@ const reducer = (postsState = [], action) => {
         case "UPDATE":
             // Go through each post and find the post that has the same id as the post that was updated on the server.  Replace that post with the updated post.
             return postsState.map((post) => post._id === action.payload ? action.payload : post);
+        case "DELETE":
+            // Show all of the posts that are not equal to the post we just deleted
+            return postsState.filter((post) => post._id !== action.payload);
         default: 
             return postsState;
     }
