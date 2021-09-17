143b170d685f27bf1413f8d3d58cb3970c833c9e
Ryan Wilson <ryan.law.wilson@gmail.com>
2021-09-17 10:37:50 -0600

    Set up server for heroku deployment

--- server/index.js
+++ server/index.js
@@ -30,6 +30,6 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+        app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port: ${PORT}`))
     })
     .catch((error) => console.log(error.message));
