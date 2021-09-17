15976633707572ac28660965ae8267d3251eca6a
Ryan Wilson <ryan.law.wilson@gmail.com>
2021-09-17 08:11:50 -0600

    Start getting project ready for Heroku

--- server/index.js
+++ server/index.js
@@ -22,6 +22,10 @@ app.use(cors());
 // This forces every route in postRoutes to start with '/posts'
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
